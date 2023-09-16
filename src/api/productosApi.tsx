f1a391cdb1e5a34beda751e6f4e0a7458a5fd6dd
Miguel Lupani <mlupani2@gmail.com>
2023-09-16 01:01:16 -0300

    changing api url

--- src/api/productosApi.tsx
+++ src/api/productosApi.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const baseURL = 'https://ecommerce-black-sigma.vercel.app/api';
+const baseURL = 'https://ecommerce-production-195b.up.railway.app/api';
 //const baseURL = 'http://192.168.0.8:8080/api';
 const productosAPI = axios.create({ baseURL });
 
